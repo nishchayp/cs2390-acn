daedfca28585001c35540814b2962c4b24145b38
Melvin He <melvin_he@brown.edu>
2023-11-03 00:09:32 -0400

    models struct

--- oniondb/oniondb.go
+++ oniondb/oniondb.go
@@ -4,16 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"cs2390-acn/models"
 )
 
 var db *sql.DB // Database connection
 
 func InitializeDB() (*sql.DB, error) {
-	var err error // Initialize error variable
+	var err error
 	db, err = sql.Open("sqlite3", "onion_router.db")
-	/*if err != nil {
+	if err != nil {
 		return nil, err
-	}*/
+	}
 
 	// Create the 'onion_data' table if it doesn't exist
 	_, err = db.Exec(`
@@ -30,34 +31,36 @@ func InitializeDB() (*sql.DB, error) {
 	return db, nil
 }
 
-func AddDataToDB(id int, ip string, port int, publicKey string) error {
+// AddDataToDB function with DirectoryEntry struct
+func AddDataToDB(entry models.DirectoryEntry) error {
+	// Open the database connection
 	onion_router_db, err := sql.Open("sqlite3", "onion_router.db")
-	/*if err != nil {
-		return nil, err
-	}*/
-	if onion_router_db == nil {
-		return fmt.Errorf("Database connection is not initialized")
+	if err != nil {
+		return err
 	}
-	
-	_, err = onion_router_db.Exec("REPLACE INTO onion_data (ID, IP, Port, PublicKey) VALUES (?, ?, ?, ?)", id, ip, port, publicKey)
+	defer onion_router_db.Close()
+
+	_, err = onion_router_db.Exec("REPLACE INTO onion_data (ID, IP, Port, PublicKey) VALUES (?, ?, ?, ?)",
+		entry.ID, entry.IP, entry.Port, entry.PublicKey)
 	return err
 }
 
-func GetDataFromDB(id int) (string, int, string, error) {
+// GetDataFromDB function with DirectoryEntry struct
+func GetDataFromDB(id int) (models.DirectoryEntry, error) {
+	// Open the database connection
 	onion_router_db, err := sql.Open("sqlite3", "onion_router.db")
-
-	if onion_router_db == nil {
-		return "", 0, "", fmt.Errorf("Database connection is not initialized")
+	if err != nil {
+		return models.DirectoryEntry{}, err
 	}
+	defer onion_router_db.Close()
 
 	row := onion_router_db.QueryRow("SELECT IP, Port, PublicKey FROM onion_data WHERE ID = ?", id)
-	var ip string
-	var port int
-	var publicKey string
-	err = row.Scan(&ip, &port, &publicKey)
+	var entry models.DirectoryEntry
+	err = row.Scan(&entry.IP, &entry.Port, &entry.PublicKey)
 	if err != nil {
-		return "", 0, "", err
+		return models.DirectoryEntry{}, err
 	}
 
-	return ip, port, publicKey, nil
+	entry.ID = id // Set the ID in the returned entry
+	return entry, nil
 }
